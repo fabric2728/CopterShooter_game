680d2705e6996409af33d8eec860e05505ce89e0
MI <MI@UNDER>
2024-05-26 17:25:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CopterShooter
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main() => Application.Run(new Form1());
+    }
+}
---
+++ Properties/Resources.Designer.cs
@@ -0,0 +1,16 @@
+﻿namespace CopterShooter.Properties
+{
+    using System.Drawing;
+    using System.Resources;
+
+    internal class Resources
+    {
+        private static readonly ResourceManager ResourceManager = new ResourceManager("Helicopter_Shooter_Game_MOO_ICT.Properties.Resources", typeof(Resources).Assembly);
+
+        public static Bitmap alien1 => (Bitmap)ResourceManager.GetObject("alien1");
+        public static Bitmap alien2 => (Bitmap)ResourceManager.GetObject("alien2");
+        public static Bitmap alien3 => (Bitmap)ResourceManager.GetObject("alien3");
+        public static Bitmap Halicopter => (Bitmap)ResourceManager.GetObject("Halicopter");
+        public static Bitmap pillar => (Bitmap)ResourceManager.GetObject("pillar");
+    }
+}
